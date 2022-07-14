e3abd4ff598bc1e2b7b9fc1d8cf659a9dd424666
bashtaleh <bashtaleh@gmail.com>
2022-07-14 16:38:05 +0430

    start

---
+++ src/App.js
@@ -0,0 +1,13 @@
+import "./App.css";
+
+function App() {
+  return (
+    <div className="App">
+      <footer>
+        This App is coded by farzane and is open-sourced in GitHub
+      </footer>
+    </div>
+  );
+}
+
+export default App;
