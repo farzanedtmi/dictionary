d37f6827ebf7e77bf716123a3b79e579deb2d0e2
bashtaleh <bashtaleh@gmail.com>
2022-07-16 20:25:48 +0430

    final changes

--- src/Meaning.js
+++ src/Meaning.js
@@ -1,5 +1,7 @@
 import React from "react";
+
 export default function Meaning(props) {
+  console.log(props.meaning);
   return (
     <div className="Meaning">
       <h3>{props.meaning.partOfSpeech}</h3>
@@ -7,9 +9,10 @@ export default function Meaning(props) {
         return (
           <div key={index}>
             <p>
-              {definition.definition} <br />
-              <em> {definition.example} </em>
-            </p>{" "}
+              {definition.definition}
+              <br />
+              <em>{definition.example}</em>
+            </p>
           </div>
         );
       })}
--- src/Results.js
+++ src/Results.js
@@ -9,8 +9,7 @@ export default function Results(props) {
         {props.results.meanings.map(function (meaning, index) {
           return (
             <div key={index}>
-              {" "}
-              <Meaning mening={meaning} />{" "}
+              <Meaning meaning={meaning} />
             </div>
           );
         })}
