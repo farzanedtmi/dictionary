cb02a5c1cb244dd186700129d2d9adf42af81504
bashtaleh <bashtaleh@gmail.com>
2022-07-16 20:18:21 +0430

    add meaning

--- src/Dictionary.js
+++ src/Dictionary.js
@@ -11,7 +11,7 @@ export default function Dictionary() {
 
   function search(event) {
     event.preventDefault();
-    alert(`Searching for ${keyword} definition`);
+
     let apiUrl = ` https://api.dictionaryapi.dev/api/v2/entries/en/${keyword}`;
     axios.get(apiUrl).then(handleRersponse);
   }
